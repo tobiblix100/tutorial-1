430ea9f12b524e9d958a8fdb41505d35f901988b
tobiblix100 <tobiblix100@gmail.com>
2017-05-11 20:29:36 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -28,8 +28,8 @@ tds = root.cssselect('td') # get all the <td> tags
 
 for td in tds:
     record = { "td" : td.text_content() } # column name and value
-        scraperwiki.sqlite.save(["td"], record) # save the records one by one
-        print td.text_content()
+    scraperwiki.sqlite.save(["td"], record) # save the records one by one
+    print td.text_content()
 # -----------------------------------------------------------------------------
 # Go back to the Tutorials page and continue to Tutorial 3 to learn about 
 # more complex scraping methods.
