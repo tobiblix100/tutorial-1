f1613b508156a0aaf80bec892201ad51d9ce9e6d
tobiblix100 <tobiblix100@gmail.com>
2017-05-11 20:26:02 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -30,11 +30,11 @@ for td in tds:
     record = { "td" : td.text } # column name and value
     try:
         scraperwiki.sqlite.save(["td"], record) # save the records one by one
-        print td.tail
+        print td.attrib['href']
     except:
         record = {"td": "NO ENTRY"}
         scraperwiki.sqlite.save(["td"], record)
-        print td.tail
+        print td.attrib['href']
 
 # -----------------------------------------------------------------------------
 # Go back to the Tutorials page and continue to Tutorial 3 to learn about 
