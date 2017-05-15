2b88687371ae12d599b088bf558b0022b123842f
tobiblix100 <tobiblix100@gmail.com>
2017-05-15 16:24:05 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,10 +22,8 @@ def scrape_and_look_for_next_link(url):
         record = {}
         table_cells = row.cssselect("td")
         if table_cells:
-            table_cells_urls = table_cells[0].cssselect("a")
             record['RaceCourse'] = table_cells[0].text_content
             record['Information'] = table_cells[1].text_content
-            record['RaceCourseURL'] = table_cells_urls.attrib.get('href')
             # Print out the data we've gathered
             print record, '------------'
             # Finally, save the record to the datastore - 'Artist' is our unique key
