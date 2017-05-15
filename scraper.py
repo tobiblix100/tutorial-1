2086a2a240e4b8bde2414a7f490adfe092f6f365
tobiblix100 <tobiblix100@gmail.com>
2017-05-15 16:04:29 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,26 +16,23 @@ def scrape_and_look_for_next_link(url):
     html = scraperwiki.scrape(url)
     print html
     root = lxml.html.fromstring(html)
-    rows = root.cssselect("table#TrolleyTable tr")  # selects all <tr> blocks within <table class="data">
+    rows = root.cssselect("table tr")  # selects all <tr> blocks within <table class="data">
     for row in rows:
         # Set up our data record - we'll need it later
         record = {}
         table_cells = row.cssselect("td")
         if table_cells:
-            table_cells_urls = table_cells[1].cssselect("a")
-            record['Date'] = table_cells[0].text
-            record['Ward total'] = table_cells[4].text
-            record['Hospital'] = table_cells[1].text
-            record['Region'] = table_cells[2].text
-            record['Trolley total'] = table_cells[3].text
-            record['HospitalURL'] = table_cells_urls[1].attrib.get('href')
+            table_cells_urls = table_cells[0].cssselect("a")
+            record['Race Course'] = table_cells[0].text_content
+            record['Information'] = table_cells[1].text_content
+            record['RaceCourseURL'] = table_cells_urls[1].attrib.get('href')
             # Print out the data we've gathered
             print record, '------------'
             # Finally, save the record to the datastore - 'Artist' is our unique key
-            scraperwiki.sqlite.save(["Hospital"], record)
+            scraperwiki.sqlite.save(["RaceCourse"], record)
 # ---------------------------------------------------------------------------
 # START HERE: define your starting URL - then 
 # call a function to scrape it.
 # ---------------------------------------------------------------------------
-starting_url = 'http://inmo.ie/6022'
+starting_url = 'http://www.ukjockey.com/racecourses.html'
 scrape_and_look_for_next_link(starting_url)
